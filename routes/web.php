df7eb300fea8df22f7f5cae0907ff0c8beb9eccb
Mohammed <Mohammed@Mohammeds-MacBook-Pro.local>
2023-12-20 17:24:51 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::view('/', 'welcome');
 
@@ -23,4 +14,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
